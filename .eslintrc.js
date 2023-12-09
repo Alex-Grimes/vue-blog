b3b8fa11eb236124d718789c623fed201b235440
alex <alexgrimes.se@gmail.com>
2023-12-08 21:17:57 -0500

    Add Vite configuration and update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
+const { computed } = require("vue");
+
 module.exports = {
     root: true,
     env: {
-      node: true
+      node: true,
+      es2022: true,
     },
     'extends': [
-      'plugin:vue/essential',
-      'eslint:recommended'
+      'plugin:vue/vue3-recommended',
+      'eslint:recommended',
+      'prettier',
     ],
     parserOptions: {
-      parser: 'babel-eslint'
+      parser: '',
     },
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
